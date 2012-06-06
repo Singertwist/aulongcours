4567a7b175872f33e7325bf0dde451417803a090
Singertwist <dark_rere@hotmail.fr>
2012-06-06 23:06:37 +0200

    Ajout des fichier footer.css/php et ajout de favicon.php

---
+++ favicon.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="js/scripts.js"></script>
+		<link rel="icon" type="image/png" href="favicon.png" />
---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="footer"> <h4>Au long cours - Tous droits réservés <br/>2012</h4>
+	<p><a href="admin.php">Connexion</a></p>
+	</div>
